bdeeba3706ce65fbbda65d7660ac5b01977cc632
fatkulumar <fatkulumar@gmail.com>
2019-04-01 23:40:07 +0700

    5 16

---
+++ app/controllers/About.php
@@ -0,0 +1,16 @@
+<?php
+
+class About extends Controller {
+    public function index($nama = "Umar", $pekerjaan = "Programmer", $umur = 22)
+    {
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $this->view('about/index', $data);
+    }
+
+    public function page()
+    {
+        $this->view('about/page');
+    }
+}
