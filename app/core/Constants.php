bbba85103cff5bc7f75f0b624d673a139baec33d
fatkulumar <fatkulumar@gmail.com>
2019-04-02 14:32:35 +0700

    model belum

---
+++ app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/umar/belajar_mvc/public/');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ini halaman about</title>
-</head>
-<body>
+<div class="container mt-5">
+    <img class="rounded-circle shadow-lg p-3 bg-black" style="width:200px" src="<?= BASEURL ?>/img/pp.jpg" alt="Nama saya">
+    <h1>Halo nama saya <?= $data['nama'] ?>, pekerjaan saya <?= $data['pekerjaan'] ?>, umur saya <?= $data['umur'] ?></h1>
+</div>
     
-<h1>Halo nama saya <?= $data['nama'] ?>, pekerjaan saya <?= $data['pekerjaan'] ?>, umur saya <?= $data['umur'] ?></h1>
-</body>
-</html>
